296404561debd17ed83bd5c2bb53d062d19d48da
IBServices <samymartins98@gmail.com>
2019-12-21 12:48:47 -0300

    Finalizando junção front com back

--- src/main/java/br/com/sig/msadmin/core/usecase/CadastrarPerfilEquipeUseCase.java
+++ src/main/java/br/com/sig/msadmin/core/usecase/CadastrarPerfilEquipeUseCase.java
@@ -1,5 +1,7 @@
 package br.com.sig.msadmin.core.usecase;
 
+import java.sql.Timestamp;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,9 @@ public class CadastrarPerfilEquipeUseCase {
 	@Autowired
 	private PerfilEquipeGateway gateway;
 	
-	public PerfilEquipeEntity cadastrarPerfilEquipe(PerfilEquipeEntity entity){
+	public PerfilEquipeEntity cadastrarPerfilEquipe(PerfilEquipeEntity entity){		
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		entity.setDataCadastro(timestamp);
 		return gateway.salvarPerfil(entity);
 	}
 	
