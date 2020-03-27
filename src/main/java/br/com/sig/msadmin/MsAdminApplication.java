dd5577cd5435bb0ab2dcd6911b95bc3d7759c778
victor212luan <streamyodin@gmail.com>
2020-03-27 17:23:49 -0300

    Adicionado metodo GET em CepFeignClient

--- src/main/java/br/com/sig/msadmin/MsAdminApplication.java
+++ src/main/java/br/com/sig/msadmin/MsAdminApplication.java
@@ -2,8 +2,10 @@ package br.com.sig.msadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsAdminApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/sig/msadmin/dataprovider/feign/CepFeignClient.java
+++ src/main/java/br/com/sig/msadmin/dataprovider/feign/CepFeignClient.java
@@ -1,5 +1,12 @@
 package br.com.sig.msadmin.dataprovider.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+
+@FeignClient(url = "${feign.viacep}", name = "viacep")
 public interface CepFeignClient {
 
+	// TODO Desenvolver a Classe EnderecoFeign 
+	//	@GetMapping("{cep}/json")
+	//	EnderecoFeign buscaEnderecoPor(@PathVariable("cep") String cep);
 }
