bf74ab9f830c916095eeba99c98fc132f250ffdf
victor212luan <streamyodin@gmail.com>
2020-05-06 16:14:33 -0300

    Adicionado regra de negócio do profissional

--- src/main/java/br/com/sig/msadmin/core/usecase/CadastrarProfissionalUseCase.java
+++ src/main/java/br/com/sig/msadmin/core/usecase/CadastrarProfissionalUseCase.java
@@ -1,5 +1,7 @@
 package br.com.sig.msadmin.core.usecase;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,11 @@ import br.com.sig.msadmin.core.entity.ProfissionalEntity;
 import br.com.sig.msadmin.core.gateway.ConsultaCepGateway;
 import br.com.sig.msadmin.core.gateway.EnderecoGateway;
 import br.com.sig.msadmin.core.gateway.ProfissionalGateway;
+import br.com.sig.msadmin.core.utils.CpfCnpjUtils;
+import br.com.sig.msadmin.core.utils.EmailUtils;
+import br.com.sig.msadmin.core.utils.IdadeUtils;
+import br.com.sig.msadmin.core.utils.RgUtils;
+import br.com.sig.msadmin.core.utils.TelefoneUtils;
 
 @Component
 public class CadastrarProfissionalUseCase {
@@ -22,6 +29,23 @@ public class CadastrarProfissionalUseCase {
 	public ProfissionalEntity cadastrarProfissional(ProfissionalEntity entity) {
 		
 		// TODO lógica da regra de negócio 
+		
+		if( !entity.getDtNasc().toString().isEmpty() && !(IdadeUtils.calcularIdade(entity.getDtNasc(), LocalDate.now()) >= 18)){
+			throw new RuntimeException("O Profissional não é maior de 18 anos");
+		} 
+		else if( !entity.getDsEmail().isEmpty() && !EmailUtils.isValid(entity.getDsEmail()) ) {
+			throw new RuntimeException("E-mail inválido, por favor preencha novamente");
+		}
+		else if( !entity.getNrRg().isEmpty() && !RgUtils.isValid(entity.getNrRg()) ){
+			throw new RuntimeException("RG inválido, por favor preencha novamente");
+		}
+		else if( !entity.getNrTelefone().isEmpty() && !TelefoneUtils.isValid(entity.getNrTelefone()) ) {
+			throw new RuntimeException("Telefone inválido, por favor preencha novamente");
+		}
+		else if( !entity.getNrCpf().isEmpty() && !CpfCnpjUtils.isValid(entity.getNrCpf()) ) {
+			throw new RuntimeException("CPF inválido! verifique novamente");
+		}
+		
 		EnderecoEntity endereco = consultaCepGateway.consultaViaCep(entity.getEndereco().getNrCep());
 		
 		endereco = enderecoGateway.cadastrarEndereco(endereco);
